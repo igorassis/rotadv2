36af777c25ab70f7feabbba18f245f569611f4bc
Igor de Assis Pires <igorpires@Igors-MacBook-Air.local>
2020-10-05 04:01:57 -0300

    feat: firebase auth integration

---
+++ src/screens/login/index.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import firebase from 'react-native-firebase';
+
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isAuth, setIsAuth] = useState(false);
+
+    const login = async () => {
+        try{
+            console.log('EMAIL =>', email)
+            console.log('password =>', password)
+            const user = await firebase.auth().signInWithEmailAndPassword(email, password);
+            setIsAuth(true);
+            console.log(user);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    return (
+        <View>
+            <TextInput
+                placeholder="Digite seu e-mail"
+                value={email}
+                onChangeText={email => setEmail(email)}
+            />
+            <TextInput
+                placeholder="Digite sua senha"
+                value={password}
+                onChangeText={Password => setPassword(Password)}
+            />
+            <TouchableOpacity onPress={login}>
+                <Text>LOGAR</Text>
+            </TouchableOpacity>
+
+            {isAuth ? <Text>LOGADO COM SUCESSO</Text> : null}
+        </View>
+    )
+}
