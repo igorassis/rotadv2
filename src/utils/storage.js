8e9630342e8f55f9eeb62d8bbc6e8db1b70e4fe2
Igor de Assis Pires <igor.pires@fretebras.com.br>
2020-10-08 09:33:27 -0300

    added uitls

---
+++ src/utils/storage.js
@@ -0,0 +1,16 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+export const storeUserData = async (user) => {
+    try{
+        const jsonUser = JSON.stringify(user)
+        await AsyncStorage.setItem('@user', jsonUser);
+    }catch(error){
+        console.log(error);
+    }
+};
+
+export const retriveUserData = async () => {
+    const user = await AsyncStorage.getItem('@user')
+    console.log('USER ==> ', JSON.parse(user));
+    return JSON.parse(user);
+};
