2c06ff0d55e5d4f935815ff207d831d48c05eaa8
Igor de Assis Pires <igor.pires@fretebras.com.br>
2020-10-09 13:36:59 -0300

    feat: update zoom to show full route on request route

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ const API_KEY = '4A8pxE3zn5R3zJp9g_ZXziLzTFGEzLNsY8SU3hnNfAc';
 const baseUrl = 'https://route.ls.hereapi.com/routing/7.2/calculateroute.json';
 
 export const getRoutes = async (route) => {
-    return await axios.get(baseUrl+`?apiKey=${API_KEY}&mode=fastest;truck&waypoint0=geo!${route.from_lat},${route.from_long}&waypoint1=geo!${route.to_lat},${route.to_long}&language=pt-br&routeattributes=sh`).then(
+    return await axios.get(baseUrl+`?apiKey=${API_KEY}&mode=fastest;truck&waypoint0=geo!${route.from_lat},${route.from_long}&waypoint1=geo!${route.to_lat},${route.to_long}&language=pt-br&routeattributes=sh,legs,bb,gr`).then(
         (res) => {
             return res;
         }
@@ -16,10 +16,12 @@ export const getRoutes = async (route) => {
 
 //PARAMETROS PARA PASSAR
 //alternatives=3
-// axleCount
 // shippedHazardousGoods
+
+
+// axleCount
 // height
 // width
 // length
 // weightPerAxle
-
+//limitedWeight
