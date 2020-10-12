a40f514899bf1b8baec738a9418f79d6d8e01955
Igor de Assis Pires <igor.pires@fretebras.com.br>
2020-10-12 20:20:50 -0300

    feat: alternatives routes

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ const API_KEY = '4A8pxE3zn5R3zJp9g_ZXziLzTFGEzLNsY8SU3hnNfAc';
 const baseUrl = 'https://route.ls.hereapi.com/routing/7.2/calculateroute.json';
 
 export const getRoutes = async (route) => {
-    return await axios.get(baseUrl+`?apiKey=${API_KEY}&mode=fastest;truck&waypoint0=geo!${route.from_lat},${route.from_long}&waypoint1=geo!${route.to_lat},${route.to_long}&language=pt-br&routeattributes=sh,legs,bb,gr`).then(
+    return await axios.get(baseUrl+`?apiKey=${API_KEY}&mode=fastest;truck&waypoint0=geo!${route.from_lat},${route.from_long}&waypoint1=geo!${route.to_lat},${route.to_long}&language=pt-br&routeattributes=sh,legs,bb,gr&alternatives=4`).then(
         (res) => {
             return res;
         }
